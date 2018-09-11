129fbb4c4842700f5ea690d5e3dddc0153ebc349
Tyler Kokoszka <techkoko77@gmail.com>
2018-09-10 23:14:39 -0400

    Update to Images and Coloring
    Uploaded a bunch of assets for the cards and the links in nav bar
    Favicon is made as well

--- src/app/components/card-tray/card-tray.component.ts
+++ src/app/components/card-tray/card-tray.component.ts
@@ -8,14 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class CardTrayComponent implements OnInit {
 
   cards = [
-    new Card('assets/img/tmp.jpg', 'C#', 'WPF, .NET, Unity', 'My big bad description you should read'),
-    new Card('assets/img/tmp.jpg', 'C#', 'WPF, .NET, Unity', 'My big bad description you should read'),
-    new Card('assets/img/tmp.jpg', 'C#', 'WPF, .NET, Unity', 'My big bad description you should read'),
-    new Card('assets/img/tmp.jpg', 'C#', 'WPF, .NET, Unity', 'My big bad description you should read'),
-    new Card('assets/img/tmp.jpg', 'C#', 'WPF, .NET, Unity', 'My big bad description you should read'),
-    new Card('assets/img/tmp.jpg', 'C#', 'WPF, .NET, Unity', 'My big bad description you should read'),
-    new Card('assets/img/tmp.jpg', 'C#', 'WPF, .NET, Unity', 'My big bad description you should read'),
-    new Card('assets/img/tmp.jpg', 'C#', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Android.png', 'Android', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Angular.png', 'Angular', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Bootstrap.png', 'Bootstrap', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/C.png', 'C', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/CPlusPlus.png', 'C++', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/CSharp.png', 'C#', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Css.png', 'CSS', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Git.png', 'Git', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Html.png', 'HTML', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Java.png', 'Java', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Javascript.png', 'Javascript', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Microsoft.png', 'Microsoft', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Nodejs.png', 'NodeJS', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/R.png', 'R', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/React.png', 'React', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/Scala.png', 'Scala', 'WPF, .NET, Unity', 'My big bad description you should read'),
+    new Card('assets/img/VBDotNet.png', 'VB.NET', 'WPF, .NET, Unity', 'My big bad description you should read'),
   ];
 
   constructor() { }
